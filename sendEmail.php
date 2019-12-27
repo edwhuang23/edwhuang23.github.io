69df9731921576853374321097a9918d456ca1df
edwhuang <edwhuang@umich.edu>
2019-12-27 18:38:18 +0800

    implement working email

--- sendEmail.php
+++ sendEmail.php
@@ -1,8 +1,6 @@
 ﻿<?php
 
-// Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
-
+$siteOwnersEmail = 'edwhuang@umich.edu';
 
 if($_POST) {
 
@@ -29,7 +27,7 @@ if($_POST) {
 
    // Set Message
    $message .= "Email from: " . $name . "<br />";
-	$message .= "Email address: " . $email . "<br />";
+   $message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= $contact_message;
    $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
